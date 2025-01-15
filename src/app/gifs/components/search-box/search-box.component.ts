909ffc65e3760812ee4d284e4dff6d326180982f
DAM2 <wowmewet1@gmail.com>
2025-01-15 16:11:17 +0100

    Subiendo cambio en docs

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.css']
 })
+
 export class SearchBoxComponent {
 
+  //esto es sin @ViewChild
+  // buscarEtiqueta(nuevaEtiqueta: string): void{
+  //   console.log(nuevaEtiqueta);
+
+  // }
+
+
+
+  @ViewChild('txtInputEtiqueta')//esto coge el valor de la etiqueta generada en el html #txtInputEtiqueta
+  public inputEtiqueta!: ElementRef<HTMLInputElement>; //esto recibe el elemento que hay en el input del html
+  //si no se pone la exclamación da error. la exclamación indica a angular que va a tener un valor si o si, por eso no hay que inicializarla
+  buscarEtiqueta(): void{
+    const nuevaEtiqueta = this.inputEtiqueta.nativeElement.value;
+    console.log({nuevaEtiqueta});
+  }
 }
